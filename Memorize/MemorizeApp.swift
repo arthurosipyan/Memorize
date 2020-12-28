a204fe457bda683d68a9e3473abca52115676f2f
Arthur Osipyan <arthurosipyan97@gmail.com>
2020-12-28 15:16:48 +0000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Arthur Osipyan on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
