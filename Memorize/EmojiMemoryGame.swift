1663c33b30ecdffb462f8352200f43a8c6fb5e41
Arthur Osipyan <arthurosipyan97@gmail.com>
2020-12-30 15:38:50 +0000

    view model are working with the latest development of the emoji memory game model

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,4 +5,28 @@
 //  Created by Arthur Osipyan on 12/29/20.
 //
 
-import Foundation
+import SwiftUI
+
+// View Model
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻","🎃","🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// Model
 struct MemoryGame<CardContent> {
     var cards: Array<Card>
     
@@ -14,9 +15,19 @@ struct MemoryGame<CardContent> {
         print("card chosen: \(card)")
     }
     
-    struct Card {
-        var isFaceUp: Bool
-        var isMatched: Bool
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
         var content: CardContent
+        var id: Int
     }
 }
